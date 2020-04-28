53220e8e32934355ef4efe730501aae86e326bef
Francisco Paulo <fpaulocgd@outlook.pt>
2020-04-28 16:03:28 +0100

    Projeto iniciado para config GitHub!

---
+++ Config.Git.Exp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Config.Git.Exp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Para configuração do GitHub conforme instruido no curso!");
+            Console.WriteLine();
+        }
+    }
+}
